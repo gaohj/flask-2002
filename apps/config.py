a1daf028b6a83e7fe93c059993aa5ff3a3d632a3
kangbazi <kangbazi@163.com>
2020-08-25 10:32:29 +0800

    model

--- apps/config.py
+++ apps/config.py
@@ -1,23 +1,41 @@
 
+import os
 
+base_dir = os.path.abspath(os.path.dirname(__file__))
+print(base_dir)
 #通用配置
 class Config(object):
-
+    SECRET_KEY = 'ADADSF1212'
     #bootstrap使用本地的静态文件
     BOOTSTRAP_SERVE_LOCAL = True
+
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_USERNAME = 'gaohj5@163.com'
+    MAIL_PASSWORD = 'TLNOFJSYQPYWISOA'
+
 
 
 #开发环境
 class DevelopmentConfig(Config):
-    pass
+    # 数据库的配置变量
+    HOSTNAME = '127.0.0.1'
+    PORT = '3306'
+    DATABASE = 'kangbazi2002'
+    USERNAME = 'root'
+    PASSWORD = '123456'
+    # 用户名:密码@数据库地址:端口号/数据库名字
+    DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
+
+    SQLALCHEMY_DATABASE_URI = DB_URI
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 #测试环境
 class TestingConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(base_dir,'testing.sqlite')
 
 #生产环境
 class ProductionConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'product.sqlite')
 
 config = {
     'test':TestingConfig,
--- apps/exts.py
+++ apps/exts.py
@@ -1,15 +1,18 @@
 #导入类库
 
 from flask_bootstrap import Bootstrap
-
+from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
 
 
 #实例化对象
 
 bootstrap = Bootstrap()
-
-
+mail = Mail()
+db = SQLAlchemy()
 #封装函数 完成初始化
 
 def config_extensions(app):
     bootstrap.init_app(app)
+    mail.init_app(app)
+    db.init_app(app)
